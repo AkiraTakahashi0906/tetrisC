bae518060fc19259eab026abd0e5786305833c98
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-03-14 13:38:52 +0900

    first2

--- tetris/Source.cpp
+++ tetris/Source.cpp
@@ -62,13 +62,193 @@ char minoShapes[MINO_TYPE_MAX][MINO_ANGLE_MAX][MINO_HEIGHT][MINO_WIDTH]{
 			0,0,0,0,
 			0,0,0,0,
 		},
-	}
+	},
 	//MINO_TYPE_O
+	{
+	//MINO_ANGLE_0
+		{
+			0,0,0,0,
+			0,1,1,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_90
+		{
+			0,0,0,0,
+			0,1,1,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_180
+		{
+			0,0,0,0,
+			0,1,1,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_270
+		{
+			0,0,0,0,
+			0,1,1,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+	},
 	//MINO_TYPE_S
+{
+	//MINO_ANGLE_0
+		{
+			0,0,0,0,
+			0,1,1,0,
+			1,1,0,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_90
+			{
+			0,1,0,0,
+			0,1,1,0,
+			0,0,1,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_180
+			{
+			0,0,0,0,
+			0,1,1,0,
+			1,1,0,0,
+			0,0,0,0,
+		},
+	//MINO_ANGLE_270
+			{
+			0,0,0,0,
+			0,1,0,0,
+			0,1,1,0,
+			0,0,1,0,
+		},
+},
 	//MINO_TYPE_Z
+	{
+		//MINO_ANGLE_0
+				{
+			0,0,0,0,
+			1,1,0,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_90
+				{
+			0,0,1,0,
+			0,1,1,0,
+			0,1,0,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_180
+				{
+			0,0,0,0,
+			1,1,0,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_270
+				{
+			0,0,0,0,
+			0,0,1,0,
+			0,1,1,0,
+			0,1,0,0,
+		},
+	},
 	//MINO_TYPE_J
+	{
+		//MINO_ANGLE_0
+				{
+			0,0,1,0,
+			0,0,1,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_90
+				{
+			0,0,0,0,
+			1,1,1,0,
+			0,0,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_180
+				{
+			0,0,0,0,
+			0,1,1,0,
+			0,1,0,0,
+			0,1,0,0,
+		},
+		//MINO_ANGLE_270
+				{
+			0,0,0,0,
+			0,1,0,0,
+			0,1,1,1,
+			0,0,0,0,
+		},
+	},
 	//MINO_TYPE_L
+	{
+		//MINO_ANGLE_0
+				{
+			0,1,0,0,
+			0,1,0,0,
+			0,1,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_90
+				{
+			0,0,0,0,
+			0,0,1,0,
+			1,1,1,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_180
+				{
+			0,0,0,0,
+			0,1,1,0,
+			0,0,1,0,
+			0,0,1,0,
+		},
+		//MINO_ANGLE_270
+				{
+			0,0,0,0,
+			0,1,1,1,
+			0,1,0,0,
+			0,0,0,0,
+		},
+	},
 	//MINO_TYPE_T
+	{
+		//MINO_ANGLE_0
+				{
+			0,1,0,0,
+			1,1,0,0,
+			0,1,0,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_90
+				{
+			0,0,0,0,
+			1,1,1,0,
+			0,1,0,0,
+			0,0,0,0,
+		},
+		//MINO_ANGLE_180
+				{
+			0,0,0,0,
+			0,1,0,0,
+			0,1,1,0,
+			0,1,0,0,
+		},
+		//MINO_ANGLE_270
+				{
+			0,0,0,0,
+			0,0,1,0,
+			0,1,1,1,
+			0,0,0,0,
+		},
+	},
 };
 int minoType = 0, minoAngle = 0;
 int minoX = 5, minoY = 0;
@@ -96,6 +276,13 @@ bool isHit(int _minoX, int _minoY, int _minoType, int _minoAngle) {
 	return false;
 }
 
+void resetMino() {
+	minoX = 5;
+	minoY = 0;
+	minoType = rand() % MINO_TYPE_MAX;
+	minoAngle = rand() % MINO_ANGLE_MAX;
+}
+
 void main() {
 
 	memset(field, 0, sizeof(field));
@@ -104,6 +291,7 @@ void main() {
 	for (int i = 0; i < FIELD_WIDTH; i++)
 		field[FIELD_HEIGHT - 1][i] = 1;
 
+	resetMino();
 	time_t t = time(NULL);
 
 	while (true)
@@ -157,11 +345,23 @@ void main() {
 				for (int i = 0; i < MINO_HEIGHT; i++)
 					for (int j = 0; j < MINO_WIDTH; j++)
 						field[minoY + i][minoX + j] |= minoShapes[minoType][minoAngle][i][j];
+				{
+					for (int i = 0; i < FIELD_HEIGHT - 1; i++) {
+						bool lineFill = true;
+						for (int j = 1; j < FIELD_WIDTH - 1; j++) {
+							if (!field[i][j])
+								lineFill = false;
+						}
+						if (lineFill)
+							//for (int j = 1; j < FIELD_WIDTH - 1; j++)
+								//field[i][j] = 0;
 
-				minoX = 5;
-				minoY = 0;
-				minoType = MINO_TYPE_I; //rand() % MINO_TYPE_MAX;
-				minoAngle = rand() % MINO_ANGLE_MAX;
+						for (int j = i; 0 < j; j--) {
+							memcpy(field[j], field[j - 1], FIELD_WIDTH);
+							}
+					}
+				}
+				resetMino();
 			}
 			else
 				minoY++;
